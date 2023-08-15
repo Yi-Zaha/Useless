3fadbd0595d6802bb8cd774e607f9e421324bb6b
136574884+Yi-Zaha@users.noreply.github.com <136574884+Yi-Zaha@users.noreply.github.com>
2023-08-16 01:17:43 +0530

    Update manga.py

--- bot/helpers/manga.py
+++ bot/helpers/manga.py
@@ -523,7 +523,7 @@ class Nhentai:
                 )
             else:
                 proxy_image_urls = [
-                    f"{proxy_url}?src={url}&referer={chapter_url}" for url in self.image_urls]
+                    f"{proxy_url}?src={url}&referer={self.url}" for url in self.image_urls]
                 graph_url = await images_to_graph(
                     title,
                     proxy_image_urls,
