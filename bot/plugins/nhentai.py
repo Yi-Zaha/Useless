a8f9721d2b0697563eabf99f190d34ed17d8f545
136574884+Yi-Zaha@users.noreply.github.com <136574884+Yi-Zaha@users.noreply.github.com>
2023-08-17 01:59:58 +0530

    Update nhentai.py

--- bot/plugins/nhentai.py
+++ bot/plugins/nhentai.py
@@ -30,7 +30,7 @@ async def generate_doujin_info(doujin, graph=False):
         )
         doujin.read_url = graph_link or doujin.url.rtrip("/") + "/1"
 
-    msg = f"[{doujin.english_title}]({doujin.read_url)\n" f"\n➤ **Code:** [{doujin.code}]({doujin.url})"
+    msg = f"[{doujin.english_title}]({doujin.read_url})\n" f"\n➤ **Code:** [{doujin.code}]({doujin.url})"
 
     if doujin.categories:
         msg += f"\n➤ **Type:** {' '.join(doujin.categories)}"
