66420bfa793eb684d697bd2481e776ab36d4e95c
Yi-Zaha <136574884+Yi-Zaha@users.noreply.github.com>
2024-07-01 20:59:37 +0530

    update hanime.py

--- bot/plugins/hanime.py
+++ bot/plugins/hanime.py
@@ -199,17 +199,18 @@ async def search_query(client, update, query=None, page=0, button_user=None, cb=
             )
 
     message = update.message if cb else update
-    if getattr(message.reply_to_message, "text", None):
-        query = (
-            message.reply_to_message.text.split(" ", maxsplit=1)[1]
-            if message.reply_to_message.text.lower().startswith("/hentai")
-            else message.reply_to_message.text
-        )
-    else:
-        await update.answer(
-            "Sorry, the bot restarted! Please redo the command.", show_alert=True
-        )
-        return
+    if not query:
+        if getattr(message.reply_to_message, "text", None):
+            query = (
+                message.reply_to_message.text.split(" ", maxsplit=1)[1]
+                if message.reply_to_message.text.lower().startswith("/hentai")
+                else message.reply_to_message.text
+            )
+        elif cb:
+            await update.answer(
+                "Sorry, the bot restarted! Please redo the command.", show_alert=True
+            )
+            return
 
     try:
         result = await HanimeTV.search(query)
