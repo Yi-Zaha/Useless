d27b703bbe3159e91bec01a1d7f6bd80a5df857a
Yi-Zaha <136574884+Yi-Zaha@users.noreply.github.com>
2024-07-02 19:40:13 +0530

    update req_track.py

--- bot/plugins/req_track.py
+++ bot/plugins/req_track.py
@@ -166,7 +166,7 @@ async def handle_requests(client, message):
 async def handle_request_action(client, callback):
     message = callback.message
     splited = callback.data.split("_")
-    re_group = message.chat.id
+    re_group = str(message.chat.id)
     req_db = await dB.get_key("REQUESTDB") or {}
     req_db.setdefault(re_group, [])
 
